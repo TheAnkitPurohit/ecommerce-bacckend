824df536863871b8db058ee27835a8f694c31611
Ankit <theankitpurohit@gmail.com>
2022-08-02 08:57:49 +0530

    authentication completed

--- models/User.js
+++ models/User.js
@@ -2,48 +2,51 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const { v4: uuidv4 } = require("uuid");
 
-const userSchmea = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength:32,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 32,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      maxLength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    userInfo: {
+      type: String,
+      trim: true,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastname: {
-    type: String,
-    trim: true,
-    maxLength:32,
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-  },
-  userInfo: {
-    type: String,
-    trim: true,
-  },
-  encry_password: {
-    type: String,
-    required: true,
-  },
-  salt: {
-    type: String,
-  },
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-}, {
-    timestamps:true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-userSchmea
-  .virtual()
+userSchema
+  .virtual("password")
   .set(function (password) {
     this._password = password;
     this.salt = uuidv4();
@@ -53,27 +56,22 @@ userSchmea
     return this._password;
   });
 
-userSchmea.methods = {
-     authenticate: function(plainpassword){
-        return this.securePassword(plainpassword) = this.encry_password
-    },
+userSchema.methods = {
+  autheticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
 
-
-    securePassword: function (plainpassword) {
-    if (!plainpassword) {
-      return "";
-    }
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
     try {
       return crypto
         .createHmac("sha256", this.salt)
         .update(plainpassword)
         .digest("hex");
-    } catch (error) {
+    } catch (err) {
       return "";
     }
   },
 };
 
-const User = mongoose.model("User", userSchmea);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const { signup, signin, signout } = require("../controllers/auth");
+const { body } = require("express-validator");
 
 // signup
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    body("name", "name should be at least 3 char").exists(), //name is required,
+    body("email", "email is required").isEmail(), // email is not valid
+    body("password", "password should be at least 5 char").isLength({ min: 5 }), // password must be at least 5 chars long
+  ],
+  signup
+);
 
 // signin
-router.post("/signin", signin);
+router.post(
+  "/signin",
+  [
+    body("email", "email is required").isEmail(), // email is not valid
+    body("password", "Password can't be empty").exists(), // password is required
+  ],
+  signin
+);
 
 // sign out
-router.post("/signout", signout);
+router.get("/signout", signout);
 
 module.exports = router;
